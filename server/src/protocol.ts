73ef3cbb435b7d0cab24b744e5c0a3ee12f4a27d
joris hermans <JHermans85@hotmail.com>
2024-08-15 22:56:46 +0200

    improve code, move more code to common

--- server/src/protocol.ts
+++ server/src/protocol.ts
@@ -1,8 +1,8 @@
 import { dispatch, Message } from "./dispatcher.ts";
-import { ProtocolEvent } from "common";
+import {Protocol} from 'common';
 
 /** This abstract class you need to inherit to set up a protocol. */
-export abstract class Protocol extends ProtocolEvent {
+export abstract class ServerProtocol extends Protocol {
     public socket?: WebSocket;
     public id: number = -1;
 
@@ -10,16 +10,7 @@ export abstract class Protocol extends ProtocolEvent {
         super()
     }
 
-    public open(id: number, socket: WebSocket): void {
-        this.socket = socket;
-        this.id = id;
-    }
-
     abstract message(id: number, message: MessageEvent): void;
-
-    public close(_id: number): void {}
-
-    public error(_id: number): void {}
 
     send(message: Message) {
         dispatch(this.id, message);
